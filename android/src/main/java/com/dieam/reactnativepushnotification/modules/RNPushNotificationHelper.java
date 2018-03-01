ddc76d14ce29476b8e03258df6f2716814e3db32
khrizt <khrizt@users.noreply.github.com>
2018-03-01 12:46:05 +0100

    Accepting messages from mixpanel

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -79,7 +79,9 @@ public class RNPushNotificationHelper {
             return;
         }
 
-        if (bundle.getString("message") == null) {
+        if (bundle.getString("message") == null && bundle.getString("mp_message") != null) {
+            bundle.putString("message", bundle.getString("mp_message"));
+        } else if (bundle.getString("message") == null) {
             Log.e(LOG_TAG, "No message specified for the scheduled notification");
             return;
         }
